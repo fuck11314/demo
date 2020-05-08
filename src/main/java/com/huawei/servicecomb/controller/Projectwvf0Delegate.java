9cf2ac6a2a703ed4692cf5fa96b4c79b7a3f8695
fuck11314 <65010679+fuck11314@users.noreply.github.com>
2020-05-08 17:44:26 +0800

    Update Projectwvf0Delegate.java

--- src/main/java/com/huawei/servicecomb/controller/Projectwvf0Delegate.java
+++ src/main/java/com/huawei/servicecomb/controller/Projectwvf0Delegate.java
@@ -9,6 +9,6 @@ public class Projectwvf0Delegate {
     public String helloworld(String name){
 
         // Do Some Magic Here!
-        return “Welcome,”name;
+        return "Welcome," +name;
     }
 }
